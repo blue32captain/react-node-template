443f59e2a9373f1e6071fed0b4ccf8dde8a89321
blue32captain <blue32captain@gmail.com>
2019-01-05 18:46:19 -0500

    yarn upgrade, removes favicon, updates api helpers

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const {resolve} = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-let FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -27,21 +26,6 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({title: 'react-node-template', template: './src/app/index.html'}),
-    new FaviconsWebpackPlugin({
-      logo: './src/app/static/favicon.png',
-      prefix: 'icons-[hash]/',
-      emitStats: false,
-      statsFilename: 'iconstats-[hash].json',
-      persistentCache: true,
-      inject: true,
-      background: '#fff',
-      title: 'template-favicon',
-      icons: {
-        favicons: true,
-        firefox: true,
-        twitter: true
-      }
-    })
   ],
   output: {
     filename: '[name].[hash].bundle.js',
